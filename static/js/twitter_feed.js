9bbaf8407b3cef912516bf804b357d1cfe190cc8
Connor Dowson <connormwdowson@gmail.com>
2018-02-08 13:35:26 +0000

    Added side twitter feed

---
+++ static/js/twitter_feed.js
@@ -0,0 +1,16 @@
+var widgetCSS = "" +
+    "body{font-family: 'Montserrat', sans-serif;}" +
+    ".timeline-Body{background-color: #333; border-bottom: 5px solid $main-green;}" +
+    ".timeline-Footer{background-color: #333; border-bottom: 5px solid $main-green;}" +
+    ".timeline-Tweet-text{color: white;}" + 
+    ".timeline-TweetList-tweet:hover{background-color: #62DD8B;}" +
+    ".timeline-Tweet:hover{background-color: #444;}"
+
+function paint(){
+  var w = document.getElementById("twitter-widget-0").contentDocument;
+  
+  var s = document.createElement("style");
+  s.innerHTML = widgetCSS;
+  s.type = "text/css";
+  w.head.appendChild(s);
+}
